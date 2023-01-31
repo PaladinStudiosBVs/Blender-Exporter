bbe2929135c10bfbb47c8f964892c42513f3b9df
Laurens <laurens@paladinstudios.com>
2023-01-31 13:03:13 +0100

    refactor: toggle icons now switch before being assigned to ui porperties

--- PaladinExporter/ui/lists.py
+++ PaladinExporter/ui/lists.py
@@ -4,23 +4,21 @@ from ..utilities.general import is_collection_valid
 
 class VIEW3D_UL_ExportList(bpy.types.UIList):
     def draw_item(self, context, layout, data, item, icon, active_data, active_propname, index):
-        include = item.include
-        icon_path = get_icon('custom_path')
-        collection_false = get_icon('collection_false')
-        collection_true = get_icon('collection_true')
-        
         row = layout.row(align=True)
-
+        
         if not is_collection_valid(item.name):
             row.enabled = False
             row.label(text=f"Missing Collection: '{item.name}'", icon='ERROR')
             return
 
-        if include:
-            row.prop(item, 'include', icon_only=True, icon='CHECKBOX_HLT', emboss=False)
-        else:
-            row.prop(item, 'include', icon_only=True, icon='CHECKBOX_DEHLT', emboss=False)
+        include = item.include
+        icon_path = get_icon('custom_path')
+        icon_collection = get_icon('collection_true') if item.use_collection else get_icon('collection_false')
+        icon_origin = 'LOCKED' if item.use_origin else 'UNLOCKED'
+        icon_include = 'CHECKBOX_HLT' if item.include else 'CHECKBOX_DEHLT'
 
+        row.prop(item, 'include', icon_only=True, icon=icon_include, emboss=False)
+        
         if item.use_path:
             split = row.split(factor=0.35)
             col_cell = split.column()
@@ -37,21 +35,9 @@ class VIEW3D_UL_ExportList(bpy.types.UIList):
             row.prop(item, 'use_path', icon_only=True, icon='RIGHTARROW_THIN', emboss=False)
         else:
             row.prop(item, 'use_path', icon_only=True, icon_value=icon_path, emboss=False)
+        row.prop(item, 'use_origin', icon_only=True, icon=icon_origin, emboss=False)
+        row.prop(item, 'use_collection', icon_only=True, icon_value=icon_collection, emboss=False)
         
-        #row.prop(item, 'use_path', icon_only=True, icon_value=icon_path, emboss=False)
-        
-        if item.use_origin:
-            row.prop(item, 'use_origin', icon_only=True, icon='LOCKED', emboss=False)
-        else:
-            row.prop(item, 'use_origin', icon_only=True, icon='UNLOCKED', emboss=False)
-        
-        if item.use_collection:
-            row.prop(item, 'use_collection', icon_only=True, icon_value=collection_true, emboss=False)
-        else:
-            row.prop(item, 'use_collection', icon_only=True, icon_value=collection_false, emboss=False)
-
-        
-            
 classes = (VIEW3D_UL_ExportList,)
 
 register, unregister = bpy.utils.register_classes_factory(classes)
