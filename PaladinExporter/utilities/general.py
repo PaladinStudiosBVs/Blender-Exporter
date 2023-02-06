5057e07d567b42df4f0e8672046ac7f7ff441c3f
Laurens <laurens@paladinstudios.com>
2023-02-06 16:52:11 +0100

    refactor: 'export_object_types' is now part of data.items

--- PaladinExporter/utilities/general.py
+++ PaladinExporter/utilities/general.py
@@ -1,4 +1,5 @@
 import bpy, json, os, sys
+from ..data.items import export_object_types
 
 def get_path():
     return os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
@@ -10,8 +11,8 @@ def preset_items_get():
     return [(
         os.path.basename(file),
         json.load(open(os.path.join(preset_path_get(), file)))["name"],
-        json.load(open(os.path.join(preset_path_get(), file)))["description"]) 
-        for file in os.listdir(preset_path_get()) if file.endswith(".json")]
+        json.load(open(os.path.join(preset_path_get(), file)))["description"]
+        ) for file in os.listdir(preset_path_get()) if file.endswith(".json")]
 
 def is_collection_valid(item_name):
     return any(collection.name == item_name for collection in bpy.data.collections)
@@ -33,16 +34,14 @@ def get_event_modifiers(event):
 def get_export_path(export_set, export_item, filename):
     return os.path.join(export_item.path or export_set.path or os.path.dirname(bpy.data.filepath), filename)
 
-object_types = ('MESH','EMPTY','ARMATURE')
-
 def exportable(obj):
-    return obj.parent == None and obj.type in object_types and obj.visible_get()
+    return obj.parent == None and obj.type in export_object_types and obj.visible_get()
 
 def exportable_selected(obj):
-    return obj.parent == None and obj.type in object_types and obj.select_get() and obj.visible_get()
+    return obj.parent == None and obj.type in export_object_types and obj.select_get() and obj.visible_get()
 
 def exportable_selected_nested(obj):
-    return obj.parent and obj.type in object_types and obj.select_get() and obj.parent.visible_get()
+    return obj.parent and obj.type in export_object_types and obj.select_get() and obj.parent.visible_get()
     
     
 
