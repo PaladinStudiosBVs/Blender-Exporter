c99d44c659c4d0e03778686a8b4ac85275386d80
Adrian Rogoz <adrian@paladinstudios.com>
2023-04-25 18:05:32 +0200

    feat: move addon source files into addons directory

---
+++ addons/PaladinExporter/operators/op_export_sets.py
@@ -0,0 +1,118 @@
+import bpy
+from bpy.props import EnumProperty, IntProperty
+
+class Paladin_OT_ExportSetAdd(bpy.types.Operator):
+    bl_idname = "paladin.export_set_add"
+    bl_label = "Add Set"
+    bl_description = "Adds an export set"
+    bl_options = {'UNDO'}
+    
+    def execute(self, context):
+        context.scene.exporter.sets.add()
+        return{'FINISHED'}
+
+class Paladin_OT_ExportSetRemove(bpy.types.Operator):
+    bl_idname = "paladin.export_set_remove"
+    bl_label = "Remove Set"
+    bl_description = "Delete this 'Export Set'"
+    bl_options = {'UNDO'}
+
+    @classmethod
+    def poll(cls, context):
+        return context.scene.exporter.sets
+
+    index:IntProperty(name="set_index", default=0)
+
+    def execute(self, context):
+        context.scene.exporter.sets.remove(self.index)
+        return{'FINISHED'}
+
+class Paladin_OT_ExportSetItemAdd(bpy.types.Operator):
+    bl_idname = "paladin.export_set_items_add"
+    bl_label = "Add"
+    bl_options = {'UNDO'}
+
+    @classmethod
+    def poll(cls, context):
+        if context.collection.name == 'Scene Collection':
+            return False
+        return True
+            
+    @classmethod
+    def description(cls, context, event):
+        collection_name = context.collection.name
+        if collection_name == 'Scene Collection':
+            return "You cannot add 'Scene Collection'"
+        return f"Adds Collection '{collection_name}' to this export set"
+
+    set_index:IntProperty(name="Set Index", default=0)
+
+    def execute(self, context):
+        collection_name = context.collection.name
+        export_set = context.scene.exporter.sets[self.set_index]
+        
+        for item in export_set.items:
+            if item.name == collection_name:
+                self.report({'WARNING'}, f"Collection '{collection_name}' already in set {self.set_index + 1}.")
+                return {'CANCELLED'}
+        
+        item = export_set.items.add()
+        item.name = collection_name
+        export_set.items_index = len(export_set.items)-1
+        return{'FINISHED'}
+
+class Paladin_OT_ExportSetItemRemove(bpy.types.Operator):
+    bl_idname = "paladin.export_set_item_remove"
+    bl_label = "Remove set item"
+    bl_options = {'UNDO'}
+    bl_description = "Removes selected Item from this set"
+
+    set_index:IntProperty(name="Set Index", default=0)
+
+    def execute(self, context):
+        export_set = context.scene.exporter.sets[self.set_index]
+        items = export_set.items
+
+        items.remove(export_set.items_index)
+        # Selects item above index when it is removed:
+        export_set.items_index = min(max(0, export_set.items_index - 1), len(items) - 1)
+        return{'FINISHED'}
+
+class Paladin_OT_ExportSetItemMove(bpy.types.Operator):
+    bl_idname = "paladin.export_set_item_move"
+    bl_label = "Move"
+    bl_options = {'UNDO'}
+
+    @classmethod
+    def description(cls, context, event):        
+        return "Move Export Set Collection up or down"
+    
+    set_index:IntProperty(name="Set Index", default=0)
+    direction: EnumProperty(items=[('UP', 'Up',""),('DOWN', 'Down',"")])
+
+    def execute(self, context):
+        export_set = context.scene.exporter.sets[self.set_index]
+        items = export_set.items
+        index = export_set.items_index
+
+        if self.direction == "UP":
+            next_index = max(index -1, 0)
+        elif self.direction == "DOWN":
+            next_index = min(index +1, len(items) -1)
+            
+        items.move(index, next_index)
+        export_set.items_index = next_index
+        return{'FINISHED'}
+
+classes = (
+    Paladin_OT_ExportSetAdd,
+    Paladin_OT_ExportSetRemove,
+    Paladin_OT_ExportSetItemAdd, 
+    Paladin_OT_ExportSetItemRemove,
+    Paladin_OT_ExportSetItemMove,
+    )
+    
+register, unregister = bpy.utils.register_classes_factory(classes)
+
+if __name__ == "__main__":
+    register()
---
+++ addons/PaladinExporter/utilities/general.py
@@ -0,0 +1,45 @@
+import bpy, json, os, sys
+from ..data.items import export_object_types
+
+def get_path():
+    return os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+
+def preset_path_get():
+    return os.path.join(get_path(), "presets")
+
+def preset_items_get():
+    return [(
+        os.path.basename(file),
+        json.load(open(os.path.join(preset_path_get(), file)))["name"],
+        json.load(open(os.path.join(preset_path_get(), file)))["description"]
+        ) for file in os.listdir(preset_path_get()) if file.endswith(".json")]
+
+def is_collection_valid(item_name):
+    return any(collection.name == item_name for collection in bpy.data.collections)
+
+def has_sets_include(export_sets):
+    return any(export_set.include for export_set in export_sets)
+
+def included_sets_has_item(export_sets):
+    return any(export_set.include and export_set.items for export_set in export_sets)
+    
+def get_event_modifiers(event):
+    ctrl = event.oskey if sys.platform.startswith('darwin') else event.ctrl
+    alt = event.alt
+    shift = event.shift
+    return ctrl, alt, shift
+
+def get_export_path(export_set, export_item, filename):
+    return os.path.join(export_item.path or export_set.path or os.path.dirname(bpy.data.filepath), filename)
+
+def exportable(obj):
+    return obj.parent == None and obj.type in export_object_types and obj.visible_get()
+
+def exportable_selected(obj):
+    return obj.parent == None and obj.type in export_object_types and obj.select_get() and obj.visible_get()
+
+def exportable_selected_nested(obj):
+    return obj.parent and obj.type in export_object_types and obj.select_get() and obj.parent.visible_get()
+    
+    
+
